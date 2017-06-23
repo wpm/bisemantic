7dd2e0d086518386dce458d336b4d0a827d4914a
W.P. McNeill <billmcn@gmail.com>
2017-06-23 13:33:49 -0500

    Add github url to setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     name="bisemantic",
     version=bisemantic.__version__,
     packages=["bisemantic"],
-    url="",
+    url="https://github.com/wpm/bisemantic",
     license="",
     entry_points={
         "console_scripts": ["bisemantic=bisemantic.console:main"],
     },
     author="W.P. McNeill",
     author_email="billmcn@gmail.com",
-    description="Text pair equivalence detector", install_requires=['pandas', 'spacy', 'keras>=2.0.5', 'numpy', 'toolz']
+    description="Text pair equivalence detector",
+    install_requires=['pandas', 'spacy', 'keras>=2.0.5', 'numpy', 'toolz']
 )
